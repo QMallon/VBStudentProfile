5b66e4a9d823f295f05fa653dc5af61ad7262317
QMallon <57999351+QMallon@users.noreply.github.com>
2020-11-19 14:30:04 -0500

    FormWork

--- VBstudentProfile/VBStudentProfileForm/My Project/Application.Designer.vb
+++ VBstudentProfile/VBStudentProfileForm/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VBStudentProfileForm.Form1
+            Me.MainForm = Global.VBStudentProfileForm.StudentProfile
         End Sub
     End Class
 End Namespace
